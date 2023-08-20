695b6f5549737d081ea3487bd84503b0085a3cb0
sayoridev <mliguori798@gmail.com>
2023-08-20 15:22:39 +0200

    add wifi check

--- script/english/package.py
+++ script/english/package.py
@@ -1,4 +1,4 @@
-import socket
+import platform
 import os
 import time
 import subprocess
@@ -7,20 +7,31 @@ spotify_package = ["flatpak", "zip", "unzip", "curl"]
 distro_list = ["1. Ubuntu", "2. Fedora", "3. Arch", "4- Void Linux", "5. Gentoo", "6. OpenSUSE", "7. CentOS"]
 os_list = ["C. ChromeOS", "m. macOS"]
 
-def check_internet_connection(host="8.8.8.8", port=53, timeout=5):
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except Exception as e:
+def check_internet_connection():
+    if platform.system() == "Linux":
+        result = subprocess.run(['nmcli', 'radio', 'wifi'], capture_output=True, text=True)
+        return "enabled" in result.stdout
+    elif platform.system() == "Darwin":
+        result = subprocess.run(['networksetup', '-getairportpower', 'Wi-Fi'], capture_output=True, text=True)
+        return "On" in result.stdout
+    else:
         return False
 
+print("I procede to check if wifi work")
+
+time.sleep(0.5)
+os.system('clear')
+
+if not check_internet_connection():
+    print("Wi-Fi is turned off. Please turn it on and relaunch the script.")
+    exit()
+
 if check_internet_connection():
-    print("I procede to chose the distro")
+    print("Wifi is work, I proceed to choose the distro")
 else:
     time.sleep(0.5)
     os.system('clear')
-    print("Connect to wifi and relaunch the script")
+    print("Connect to Wi-Fi and relaunch the script")
     os.system('exit')
 
 print("What distro are you using?")
@@ -36,6 +47,3 @@ print(os_list[0])
 print(os_list[1])
 
 scelta=input()
-
-
-
