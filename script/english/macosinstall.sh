db7cc0ca8b5607b892c692cd826a4ecf3975afa2
sayoridev <mliguori798@gmail.com>
2023-08-21 19:01:56 +0200

    fix installer for macOS

--- script/english/macosinstall.sh
+++ script/english/macosinstall.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 chmod +x script/english/macosinstall.sh
 echo "I proceed to install the packages for patch Spotify"
 sleep 1.5
 wait
 clear
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install curl zip unzip git
+xcode-select --install
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo "Brew and need package successfully installed"
 sleep 1.5
 wait
@@ -18,5 +20,7 @@ hdiutil attach Spotify.dmg
 cp -R /Volumes/Spotify/Spotify.app /Applications/
 hdiutil detach /Volumes/Spotify
 clear
-echo "i procede to patch Spotify"
-bash <(curl -sSL https://gist.github.com/jetfir3/e8830cf8deba6a4f15eec094d344f7b1/raw/spotx.sh) -Bcd
+echo "I proceed to patch Spotify"
+curl -sSL https://gist.github.com/jetfir3/e8830cf8deba6a4f15eec094d344f7b1/raw/spotx.sh > spotx.sh
+bash spotx.sh -Bcd
+rm spotx.sh
