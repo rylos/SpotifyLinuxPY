a866fb90a07470ec66570cf0e53f6de0253d120d
sayoridev <mliguori798@gmail.com>
2023-08-21 18:04:37 +0200

    fix start python script

--- script/english/install.py
+++ script/english/install.py
@@ -1,22 +1,20 @@
-import os 
+import os
 import time
 import subprocess
 
-print("What do you want do?")
+print("What do you want to do?")
 print("--------------------")
 print("1. Install")
 print("2. Update")
 print("\nQ. Exit")
 
-import os
-
 install_path = os.path.join('script', 'english', 'package.py')
 update_path = os.path.join('script', 'english', 'update.py')
 
 install = os.path.join(os.getcwd(), install_path)
-update = os.path.join(os.getcwdb(), update_path)
+update = os.path.join(os.getcwd(), update_path)
 
-inp=input()
+inp = input()
 
 if inp == "1":
     with open(install, "r") as file:
@@ -32,7 +30,7 @@ elif inp == "Q":
     os.system('clear')
     os.system('exit')
 else:
-    print("Is not a valid choise")
+    print("Is not a valid choice")
     time.sleep(0.5)
     os.system('clear')
     os.system('exit')
