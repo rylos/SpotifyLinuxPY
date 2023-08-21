39b4363b2063c5045bb8a48f0efda16db9e07263
sayoridev <mliguori798@gmail.com>
2023-08-21 18:36:25 +0200

    start italy develop

--- script/italy/install.py
+++ script/italy/install.py
@@ -1,1 +1,12 @@
 import os
+import time
+import subprocess
+
+print("What do you want do?")
+print("--------------------")
+print("1. Install")
+print("2. Update")
+print("\nQ. Exit")
+
+install_path = os.path.join('script', 'english', 'package.py')
+update_
