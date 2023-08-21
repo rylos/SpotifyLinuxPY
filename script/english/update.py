d5ad2d9f8bea5a3c3daab7a32b5a80f778ef51db
sayoridev <mliguori798@gmail.com>
2023-08-21 18:33:07 +0200

    add update

---
+++ script/english/update.py
@@ -0,0 +1,12 @@
+import subprocess
+
+spotify_update_package = "com.spotify.Client"
+
+update_command = ["flatpak", "update", spotify_update_package]
+result = subprocess.run(update_command, capture_output=True, text=True)
+
+if result.returncode == 0:
+    print("Package update successful.")
+else:
+    print("Package update failed.")
+    print("Error:", result.stderr)
